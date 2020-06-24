1c1eca633db619414a46649fb65f3c63aadc5e89
Mayank Shetty <45304614+msmayankshetty99@users.noreply.github.com>
2020-06-24 17:05:46 +0530

    Adding DB Models

--- models/Gigs.js
+++ models/Gigs.js
@@ -2,24 +2,42 @@ const Sequelize = require('sequelize');
 const db = require('../config/database');
 
 const Gig = db.define('gig', {
+  id: {
+    type: Sequelize.INTEGER(11),
+    primaryKey: true,
+    autoIncrement: true
+  },
   title: {
     type: Sequelize.STRING
   },
-  technologies: {
-    type: Sequelize.STRING
+  category_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false
   },
-  description: {
+  comp_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  desc: {
     type: Sequelize.STRING
   },
   budget: {
     type: Sequelize.STRING
   },
-  contact: {
-    type: Sequelize.STRING
+  status: {
+    type: Sequelize.BOOLEAN
+  },
+  timestamp: {
+    type: Sequelize.DATE
   }
 });
 
-Gig.sync().then(() => {
-  console.log('table created');
-});
+Gig.associate = function(models) {
+  
+}
+
+// Gig.sync().then(() => {
+//   console.log('table created');
+// });
+
 module.exports = Gig;
